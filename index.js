9fa770655711115c2fb4500444ee6c4193b7ca7e
Skilbhumen <virusfuf@gmail.com>
2023-02-13 19:00:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const userRoute = require("./routes/user")
+
+dotenv.config();
+
+//mongodb connection url
+mongoose.connect(process.env.MONGO_URL
+).then(()=> console.log("DB connected")).catch((err)=>
+    console.log(err)
+);
+
+// access http requests
+
+app.use("/api/user", userRoute);
+
+
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("server is running");
+})
+
