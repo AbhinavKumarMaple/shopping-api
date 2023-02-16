09f6e3c6f88bd961dbf8c369470796dd5e72ce40
unknown <virusfuf@gmail.com>
2023-02-17 01:14:05 +0530

    new update

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const orderRoute = require("./routes/order");
+const cartRoute = require("./routes/cart");
 
 dotenv.config();
 
@@ -20,6 +22,8 @@ app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/carts", cartRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("server is running");
